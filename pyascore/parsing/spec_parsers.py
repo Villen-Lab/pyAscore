2a40a0eaffa680de9fbde59d76322b3c7ece5207
Anthony Valente <valenta4@uw.edu>
2022-03-03 15:36:34 -0800

    Update doc strings for spectra parsers.

--- pyascore/parsing/spec_parsers.py
+++ pyascore/parsing/spec_parsers.py
@@ -5,11 +5,36 @@ from pyteomics.mzxml import MzXML
 
 
 class SpectraExtractor:
+    """
+    Abstract class providing a common callable to transform scans from pyteomics
+
+    Different spectra file formats provide scan objects with specific schemas that
+    are not necessarily cross compatible. To make it easy to take scans from any
+    file format, classes that inherit from this class provide a series of methods
+    to return specific attributes from scans. This allows the class to transform
+    scans into a common schema containing only the information necessary for scoring.
+    """
     def __init__(self):
         self.scan = None
         self.results = None
 
     def extract(self, scan):
+        """Extract information from a pyteomics scan dictionary
+
+        This function can be passed to the pyteomics map method to extract useful
+        informations from scans. Derivitive classes call internal methods to
+        retrieve specific information in file format specific queries.
+
+        Parameters
+        ----------
+        scan : dict
+            Scan dictionary passed by pyteomics
+
+        Returns
+        -------
+        dict
+            Scan information with common schema
+        """
         self.scan = scan
         
         self.results = {}
@@ -23,6 +48,13 @@ class SpectraExtractor:
 
 class MzMLExtractor(SpectraExtractor):
     def _get_scan(self):
+        """Extract scan number from scan
+
+        Returns
+        -------
+        int
+            Scan number
+        """
         try:
             scan_string = re.search("(?<=scan=)[0-9]+", self.scan["id"]).group()
             return int(scan_string)
@@ -30,12 +62,26 @@ class MzMLExtractor(SpectraExtractor):
             return -1
 
     def _get_ms_level(self):
+        """Extract MSn level from scan, normally 1 or 2
+
+        Returns
+        -------
+        int
+            MSn level
+        """
         try:
             return self.scan["ms level"]
         except KeyError:
             return 0
 
     def _get_precursor(self):
+        """Extract precursor m/z and charge state from scan
+
+        Returns
+        -------
+        (float, int)
+            Tuple with precursor m/z (0) and charge state (1)
+        """
         try:
             precursor_list = self.scan["precursorList"]
             nprecursors = precursor_list["count"]
@@ -49,6 +95,13 @@ class MzMLExtractor(SpectraExtractor):
             return None, None
 
     def _get_spectra(self):
+        """Extract fragment m/z array and intensity array from scan
+
+        Returns
+        -------
+        (np.ndarray, np.ndarray)
+            Tuple with fragment m/z array (0) and intensity array (1)
+        """
         try:
             return self.scan["m/z array"], self.scan["intensity array"]
         except:
@@ -57,18 +110,39 @@ class MzMLExtractor(SpectraExtractor):
 
 class MzXMLExtractor(SpectraExtractor):
     def _get_scan(self):
+        """Extract scan number from scan
+
+        Returns
+        -------
+        int
+            Scan number
+        """
         try:
             return int(self.scan["num"])
         except KeyError:
             return -1
 
     def _get_ms_level(self):
+        """Extract MSn level from scan, normally 1 or 2
+
+        Returns
+        -------
+        int
+            MSn level
+        """
         try:
             return int(self.scan["msLevel"])
         except KeyError:
             return 0
 
     def _get_precursor(self):
+        """Extract precursor m/z and charge state from scan
+
+        Returns
+        -------
+        (float, int)
+            Tuple with precursor m/z (0) and charge state (1)
+        """
         try:
             precursor_list = self.scan["precursorMz"]
             nprecursors = len(precursor_list)
@@ -81,6 +155,13 @@ class MzXMLExtractor(SpectraExtractor):
             return None, None
 
     def _get_spectra(self):
+        """Extract fragment m/z array and intensity array from scan
+
+        Returns
+        -------
+        (np.ndarray, np.ndarray)
+            Tuple with fragment m/z array (0) and intensity array (1)
+        """
         try:
             return self.scan["m/z array"], self.scan["intensity array"]
         except:
@@ -89,7 +170,27 @@ class MzXMLExtractor(SpectraExtractor):
 
 class SpectraParser:
     """
-    Parser for spectra information coming from mzMLs or mzXMLs.
+    Parser to read spectra from mzML and mzXML files
+
+    This class is designed to provide ease of access to spectra from popular file formats.
+    By providing the file of interest and the MSn level, the user can receive spectra in
+    either list form or dictionary form depending on need. Spectral entries from individual
+    file formats are normalized to a single output schema, which has the information
+    necessary for running pyAscore. This output can be further filtered by supplying a
+    custom filter, and future versions will make this option more powerful.
+
+    Parameters
+    ----------
+    spec_file_name : str
+        Path to spectral file
+    spec_file_type : str
+        The spectra file's type. One of mzML or mzXML
+    ms_level : int
+        MSn level to be returned to the user
+    custom_filter : callable
+        A callable which takes a spectral object and returns a boolean which states whether
+        a spectra should be retained. The spectral objects passed to this parameter are
+        currently the same as the ones returned to the user
     """
     def __init__(self,
                  spec_file_name,
@@ -126,6 +227,14 @@ class SpectraParser:
         self._spectra = []
 
     def _passes_filtering(self, entry):
+        """Internal function to decide whether a spectra should be retained
+        
+        Returns
+        -------
+        bool
+            True if scan should be retained, else False
+        """
+
         passes_ms_level = True
         if self.ms_level:
             passes_ms_level = entry["ms_level"] == self.ms_level
@@ -137,17 +246,33 @@ class SpectraParser:
         return passes_ms_level and passes_custom_filter
 
     def _get_spectra(self):
-        """
-        Internal function to build list of scans sorted by scan number       
-        """
+        """Internal function to build list of scans sorted by scan number"""
+
         if not self._spectra:
-            self._spectra = [s for s in self._reader.map(self._extractor.extract) if self._passes_filtering(s)]
+            self._spectra = [s for s in self._reader.map(self._extractor.extract)
+                             if self._passes_filtering(s)]
             self._spectra = sorted(self._spectra, key=lambda s: s["scan"])
 
     def to_list(self):
+        """Return spectra from file in list form
+        
+        Returns
+        -------
+        list
+            List of scans from file sorted by scan number
+        """
+
         self._get_spectra()
         return self._spectra
 
     def to_dict(self):
+        """Return spectra from file in dictionary form
+        
+        Returns
+        -------
+        dict
+            Dict of scans from file with schema: {scan number : spectra}
+        """
+
         self._get_spectra()
         return {spec.pop("scan") : spec for spec in self._spectra} 
