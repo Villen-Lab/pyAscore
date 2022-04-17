d23d57f8352e06c2665cb5679cbfd0bb44b120f1
Anthony Valente <valenta4@uw.edu>
2022-04-17 14:30:19 -0700

    Modernize package layout
    This commit focuses on laying out package meta data in adjacent
    toml files that pip can utilize at build and installation.

--- pyascore/__init__.py
+++ pyascore/__init__.py
@@ -1,2 +1,19 @@
+"""Initialize the mokapot package."""
+try:
+    from importlib.metadata import version, PackageNotFoundError
+
+    try:
+        __version__ = version(__name__)
+    except PackageNotFoundError:
+        pass
+
+except ImportError:
+    from pkg_resources import get_distribution, DistributionNotFound
+
+    try:
+        __version__ = get_distribution(__name__).version
+    except DistributionNotFound:
+        pass
+
 from .ptm_scoring import PyBinnedSpectra, PyModifiedPeptide, PyFragmentGraph, PyAscore, PyLogMath, PyBinomialDist, PyPowerSetSum
 from .parsing import *
